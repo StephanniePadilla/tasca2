d0c8f92a19e69350024694f0d815a008e1b36453
steph <stephypadilla08@gmail.com>
2018-06-17 10:38:11 +0200

    Initial commit

---
+++ app/src/main/java/com/example/steph/tasca2sarapadilla/PartidaList.java
@@ -0,0 +1,13 @@
+package com.example.steph.tasca2sarapadilla;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PartidaList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_partida_list);
+    }
+}
